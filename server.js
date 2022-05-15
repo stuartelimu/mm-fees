eb883d4f878beaae4edf08f83402429a859b608a
Stuart Elimu <stuartelimu@gmail.com>
2022-05-15 06:35:52 +0300

    add form validation

--- server.js
+++ server.js
@@ -4,6 +4,8 @@
 const express = require('express');
 const path = require('path');
 const serveIndex = require('serve-index');
+const bodyParser = require('body-parser');
+
 
 /**
  * App Variables
@@ -19,15 +21,34 @@ const port = process.env.PORT || "5000";
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true}));
 
+app.use((err, req, res, next) => {
+  res.status(404).send(`sorry I can't find that`);
+})
 
 /**
  * Routes Definition
  */
 
 app.get('/', (req,res) => {
-  res.render('index', {title: "Home"})
+  res.render('index', {
+    
+    data: {},
+    errors: {}
+  });
 });
+
+app.post('/', (req, res) => {
+  res.render('index', {
+    data: req.body,
+    errors: {
+      amount: {
+        msg: "An amount is required"
+      }
+    }
+  })
+})
 
 /**
  * Server Activation
